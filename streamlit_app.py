8a6297bf09e5d77cecab61beb4aaca4d8aa52993
OussHBZ <113288914+OussHBZ@users.noreply.github.com>
2024-06-11 15:41:30 +0000

    Change the name

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ import time
 import zipfile
 
 # Page title
-st.set_page_config(page_title='ML Model Building', page_icon='🤖')
+st.set_page_config(page_title='pca-&-ensemble', page_icon='🤖')
 st.title('🤖 ML Model Building')
 
 with st.expander('About this app'):
